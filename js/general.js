fcc50ae4d536af7f404bb5f8d74130fcec6a3772
Marcelo Rusconi <mgrusconi@gmail.com>
2014-06-09 01:47:03 -0300

    fin del trabajo

--- js/general.js
+++ js/general.js
@@ -1,28 +1,12 @@
 (function ($) {
 
 	$(document).ready(function() {
-
-		$('.popup-gallery').magnificPopup({
-			delegate: 'a',
-			type: 'image',
-			tLoading: 'Loading image #%curr%...',
-			mainClass: 'mfp-img-mobile',
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-			},
-			image: {
-				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			}
-		});
-
+		
 		$('#btn-borrar').click(function(){
 			$('.input-text').val('');
 			$('#form-consulta').val('');
 			return false;
 		});
-
 		$('.porton-sistema-corredizo').show();
 		$('.cortinas-plana').show();
 
